80715e3940e80df75e3ba80b33b61923a36db49f
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-12-01 17:01:33 +0700

    Add project files.

---
+++ Activity6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace StackUsingLinkedList_CSharp
+{
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+}
